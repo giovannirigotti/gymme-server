e9cd49d41e4222730ac9a7fb8d926882188b751d
giovannirigotti <rigotti.giovanni@gmail.com>
2021-01-25 14:42:23 +0100

    aggiunta funzione per registrazione delle palestre

--- index.js
+++ index.js
@@ -98,6 +98,89 @@ app.post('/customer/register/', (req, res, next) => {
   });
 })
 
+app.post('/gym/register/', (req, res, next) => {
+  console.log("Rispondo richiesta:'/gym/register/");
+  var to_add = req.body;
+
+  //USERS
+  var name = to_add.name;
+  var lastname = to_add.lastname;
+  var birthdate = to_add.birthdate;
+  var email = to_add.email;
+  var password = to_add.password;
+  var user_type = to_add.user_type;
+  //GYMS
+  var vat_number = to_add.vat_number;
+  var gym_name = to_add.gym_name;
+  var gym_address = to_add.gym_address;
+  var zip_code = to_add.zip_code;
+  var pool = to_add.pool;
+  var box_ring = to_add.box_ring;
+  var aerobics = to_add.aerobics;
+  var spa = to_add.spa;
+  var wifi = to_add.wifi;
+  var parking_area = to_add.parking_area;
+  var personal_trainer_service = to_add.personal_trainer_service;
+  var nutritionist_service = to_add.nutritionist_service;
+  var impedance_balance = to_add.impedance_balance;
+  var courses = to_add.courses;
+  var showers = to_add.showers;
+
+  //CONTROLLO CHE NON CI SIA GIA' NEL DB
+  var check_query = "SELECT * FROM users WHERE email = \'" + email +"\';";
+  con.query(check_query, function(err, result, fields){
+    if(err){
+      res.json("500");
+      console.log('[PostgreSQL ERROR]', err);
+    }else {
+      if(result.rowCount > 0){
+        res.statusCode=500;
+        res.json('User already exists!');
+      }
+      else{
+        //INSERISCO DATI NELLA TABELLO users
+        var register_query = "INSERT INTO users (name, lastname, birthdate, email, password, user_type) VALUES ('"+name+"', '"+lastname+"', TO_DATE('"+birthdate+"', 'DD/MM/YYYY'), '"+email+"', '"+password+"', '"+user_type+"');";
+        con.query(register_query, function(err, result, fields){
+
+          if(err){
+            res.json("500");
+            console.log('[PostgreSQL ERROR]', err);
+          }else {
+            //PRENDO user_id DELLO USER APPENA INSERITO
+            var userID_query = "SELECT user_id FROM users WHERE email = \'" + email +"\';";
+            con.query(userID_query, function(err, result, fields){
+              if(err){
+                res.json("500");
+                console.log('[PostgreSQL ERROR]', err);
+              }else {
+                if(result.rowCount > 0){
+                  var user_id = result.rows[0].user_id;
+
+                  //AGGIUNGO DATI SPEFICI DEL customer
+                  var user_query = "INSERT INTO gyms (user_id, vat_number, gym_name, gym_address ,zip_code ,pool, box_ring, aerobics, spa, wifi, parking_area, personal_trainer_service, nutritionist_service, impedance_balance, courses, showers) VALUES ('"+user_id+"','"+vat_number+"','"+gym_name+"','"+gym_address+"','"+zip_code+"','"+pool+"','"+box_ring+"','"+aerobics+"','"+spa+"','"+wifi+"','"+parking_area+"','"+personal_trainer_service+"','"+nutritionist_service+"','"+impedance_balance+"','"+courses+"','"+showers+"');";
+                  con.query(user_query, function(err, result, fields){
+                    if(err){
+                      res.json("500");
+                      console.log('[PostgreSQL ERROR]', err);
+                    }else {
+                      //SUCCESS FINALE
+                      res.json("200");
+                    }
+                  });
+
+                } else{
+                  res.json("500");
+                  console.log('[Register ERROR]', err);
+                }
+              }
+            });
+          }
+        });
+      }
+    }
+  });
+})
+
 app.post('/login/', (req, res, next) => {
   console.log("Rispondo richiesta:'/login/");
   var to_check = req.body;
